6cfc299782a2256f9ebd1e6903e1fbe6737338fd
unknown <myrnamares90@gmail.com>
2019-08-22 19:46:46 -0500

    se elimino código muerto

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './App.css';
 import { Route, Switch, HashRouter } from 'react-router-dom';
-import PostForm from './components/PostForm';
-import AllPost from './components/AllPost';
 import Home from './components/Home';
 
 function App() {
